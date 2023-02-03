a01036400633aee8a8e81bdab3c5feffa1a596d8
roshangangule <roshangangule@gmail.com>
2023-02-03 11:30:50 +0530

    initial commit

---
+++ src/main/java/com/librarycommon/LibraryCommonApplication.java
@@ -0,0 +1,13 @@
+package com.librarycommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryCommonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/librarycommon/LibraryCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.librarycommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
