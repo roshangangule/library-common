a91d86b5cab8e7c3c706883dd569d7283f33554f
roshangangule <roshangangule@gmail.com>
2023-02-14 09:52:17 +0530

    Pagination added and some more feature or api's

--- src/main/java/com/librarycommon/dao/BookRepository.java
+++ src/main/java/com/librarycommon/dao/BookRepository.java
@@ -4,9 +4,13 @@ import com.librarycommon.entity.Book;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book,Long> {
-    Page<Book> findAll(Pageable pageable);
-    List<Book> findAll();
+
+    Page<Book> findByTitleContaining(@RequestParam String title, Pageable pageable);
+
+    Page<Book> findByCategory(@RequestParam("category") String category, Pageable pageable);
 }
