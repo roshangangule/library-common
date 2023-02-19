bb35c9d9dd56951d31b320f29bf7cd7defa62a53
roshangangule <roshangangule@gmail.com>
2023-02-19 18:57:20 +0530

    Revert "Review repository added for get review by book id api"
    This reverts commit c098192d1c5299787c36313d1475624a1b6cdc44.

--- src/main/java/com/librarycommon/entity/Review.java
+++ src/main/java/com/librarycommon/entity/Review.java
@@ -1,26 +1,9 @@
 package com.librarycommon.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.Data;
-import org.hibernate.annotations.CreationTimestamp;
 
-import java.util.Date;
-
-@Entity
-@Table(name = "review")
 @Data
 public class Review {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String userEmail;
-    @CreationTimestamp
-    private Date date;
-    private double rating;
-    private Long bookId;
-    private String reviewDescription;
-
-
-
 }
